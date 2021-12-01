c0ffeefb3cabb07da0be3b06c7661ec0629f85e9
Atilla <atilla.mehmed@lightsoftbulgaria.com>
2021-12-01 17:45:59 +0200

    dailydata added

--- componens/elements/DailyData.js
+++ componens/elements/DailyData.js
@@ -4,10 +4,13 @@ import { View } from "react-native";
 const DailyData = ({ info }) => {
 
 
-    return (<View style={styles.hourlyView}>
+    return (<View style={styles.dailyView}>
         <Text style={styles.timeView}>{new Date(info.dt)}</Text>
-        <Text style={styles.tempView}>{info.temp}</Text>
-        <Text style={styles.tempView}>{info.feels_like}</Text>
+        <Text>Sunrise at: {new Date(info.sunrise)}</Text>
+        <Text>Sunset at: {new Date(info.sunset)}</Text>
+        <Text>Moonrise at: {new Date(info.moonrise)}</Text>
+        <Text>Moonset at: {new Date(info.moonset)}</Text>
+        <View></View>
     </View>)
 }
 
